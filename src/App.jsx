780f232d472d7f79f956ed530540a955f2327e1b
강세아 <gangsea817@gmail.com>
2026-03-04 21:50:41 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>{/* <Route path="/" element={<여기에 페이지 이름 />} /> */}</Routes>
+  );
+}
+
+export default App;
