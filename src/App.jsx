6719e8762e31d684b9e09c62c12789d2e27109be
pbc1001 <mintcho0328@gmail.com>
2026-03-13 20:24:18 +0900

    fix: 날아갔던 파일들 재정렬..

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main.jsx"
+import Login from "./pages/LogIn.jsx"
+import Signup from "./pages/SignUp.jsx"
 
 function App() {
   return (
-    <Routes><Route path="/" element={<Main />} /></Routes>
+    <Routes>
+      <Route path="/" element={<Main/>} />
+      <Route path="/Login" element={<Login/>}/>
+      <Route path="/Signup" element={<Signup/>}/>
+    </Routes>
   );
 }
 
