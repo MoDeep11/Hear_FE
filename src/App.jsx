6992be1018e3041576fb2ccde026777adc470814
강세아 <gangsea817@gmail.com>
2026-03-28 21:26:22 +0900

    feat: 경로 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main.jsx";
 import Login from "./pages/LogIn.jsx";
 import Signup from "./pages/SignUp.jsx";
-import AiChat from "./pages/AiChat";  
-import Photo_book from "./pages/PhotoBook.jsx"
-import Mypage from "./pages/MyPage.jsx"
-import Statics from "./pages/Statics.jsx"
+import AiChat from "./pages/AiChat";
+import Diary from "./pages/Diary.jsx";
+// import AiChat from "./pages/AiChat";
+import Photo_book from "./pages/PhotoBook.jsx";
+import Mypage from "./pages/MyPage.jsx";
+import Statics from "./pages/Statics.jsx";
+import Home from "./pages/Home.jsx";
 
 function App() {
   return (
@@ -14,9 +17,13 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/ai/chats" element={<AiChat />} />
-      <Route path="/photo" element={<Photo_book />} />
-      <Route path="/mypage" element={<Mypage />} />
-      <Route path="/statics" element={<Statics />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Signup" element={<Signup />} />
+      {/* <Route path="/ai/chats" element={<AiChat />} /> */}
+      <Route path="/Mypage" element={<Mypage />} />
+      <Route path="/Statics" element={<Statics />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
