c6fb0de7ecb3658b2228b9d5b974823c0fd90607
pbc1001 <mintcho0328@gmail.com>
2026-04-03 01:13:59 +0900

    chore: 로그인 및 회원가입 구현을 위한 준비
    - login.js -> auth.js로 변경
    - instance.js에 토큰 관련 추가
    - 유저 이미지 추가

---
+++ src/apis/auth.js
@@ -0,0 +1,22 @@
+import instance from "./instance";
+
+export const sendAuthcode = async (email, type) => {
+    const response = await instance.post('/api/v1/auth/email', {email, type});
+    return response.data;
+}
+
+export const authCode = async (email, code, type) => {
+    const response = await instance.post('/api/v1/auth/email-tickets', { email, code, type });
+    return response.data;
+}
+
+export const signUp = async (userData) => {
+    console.log(userData)
+    const response = await instance.post('/api/v1/auth/register', userData);
+    return response.data;
+}
+
+export const login = async (userData) => {
+  const response = await instance.post('/api/v1/auth/login', userData);
+  return response.data; 
+};
