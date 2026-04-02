b1be140c702c2de778fe64864ef011b5dc761c47
pbc1001 <mintcho0328@gmail.com>
2026-04-03 01:14:20 +0900

    feat: 회원가입 로직 구현

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -8,6 +8,7 @@ import Arrow from "../assets/Arrow.svg";
 import CheckPassword from "../assets/SeePass.svg";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { sendAuthcode, authCode, signUp } from "../apis/auth.js";
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -16,57 +17,99 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
   const [checkpw, setCheckpw] = useState("");
   const [authcode, setAuthcode] = useState("");
+  const [verifyToken, setVerifyToken] = useState("");
   const [showPw, setShowPw] = useState(false);
   const [showCode, setShowCode] = useState(false);
   const [timeleft, setTimeleft] = useState(180);
+  const [message, setMessage] = useState("");
+  const [checkMessage, setCheckMessage] = useState("");
+  const [pwMessage, setPwMessage] = useState("");
+  const [checkPwMessage, setCheckPwMessage] = useState("");
 
   useEffect(() => {
     let timer;
     if (showCode && timeleft > 0) {
       timer = setInterval(() => {
-        setTimeleft((prev) => prev - 1)
-      }, 1000)
+        setTimeleft((prev) => prev - 1);
+      }, 1000);
     } else if (timeleft === 0) {
-      alert("인증 시간이 만료되었습니다. 다시 시도해주세요.")
-      setShowCode(false)
-      setTimeleft(180)
+      alert("인증 시간이 만료되었습니다. 다시 시도해주세요.");
+      setShowCode(false);
+      setTimeleft(180);
     }
-    return () => clearInterval(timer)
-  }, [showCode, timeleft])
+    return () => clearInterval(timer);
+  }, [showCode, timeleft]);
 
   const formatTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60)
-    const remainingSeconds = seconds % 60
-    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`
-  }
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
 
-  const handleSendCode = () => {
-    setTimeleft(180)
-    setShowCode(true) 
-  }
+  const handleSendCode = async () => {
+    setTimeleft(180);
+    setShowCode(true);
+    setCheckMessage("");
+    const type = "REGISTER";
+    try {
+      await sendAuthcode(email, type);
 
-  const request_body = {
-    email: email,
-    password: password,
-    confirmPassword: checkpw
-  }
+      setCheckMessage("인증 코드가 발송되었습니다!");
+    } catch (error) {
+      alert("발송 실패: " + error.response?.data?.message);
+    }
+  };
 
-  const handleSubmit = async () => {
+  const check_Code = async () => {
+    setCheckMessage("");
+    const type = "REGISTER";
+    try {
+      const response = await authCode(email, authcode, type);
+      setVerifyToken(response.data);
+      setCheckMessage("인증에 성공했습니다!");
+      setShowCode(false);
+    } catch (error) {
+      setCheckMessage("번호를 확인해 주세요.");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessage("");
+    setPwMessage("");
+    setCheckMessage("");
+    setCheckPwMessage("");
+    console.log(verifyToken);
+    const request_body = {
+      email: email,
+      password: password,
+      confirmPassword: checkpw,
+      ticket: verifyToken,
+    };
     if (!password && !email) {
-      alert("이메일과 비밀번호를 입력해주세요");
+      setMessage("이메일을 입력해주세요");
+      setPwMessage("비밀번호를 입력해주세요");
       return;
     } else if (!password) {
-      alert("비밀번호를 입력해주세요");
+      setPwMessage("비밀번호를 입력해주세요");
       return;
     } else if (!email) {
-      alert("이메일을 입력해주세요");
+      setMessage("이메일을 입력해주세요");
       return;
     } else if (password != checkpw) {
-      alert("비밀번호가 일치하지않아요!");
+      setCheckPwMessage("비밀번호가 일치하지않아요!");
       return;
-    } else {
-      alert("회원가입 성공!");
+    } else if (!verifyToken) {
+      setCheckMessage("이메일 인증을 먼저 완료해주세요.");
+      return;
+    }
+
+    try {
+      await signUp(request_body);
+      alert("회원가입 완료! 로그인 페이지로 이동합니다.");
       navigate("/login");
+    } catch (error) {
+      alert("회원가입 실패: " + error.response?.data?.message);
     }
   };
 
@@ -93,18 +136,27 @@ const SignUp = () => {
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
                     ></Email_text>
-                    <Email_check_btn onClick={handleSendCode}>코드 발송</Email_check_btn>
+                    <Email_check_btn onClick={handleSendCode}>
+                      코드 발송
+                    </Email_check_btn>
                   </Email_input>
+                  <Email_check_text>{message}</Email_check_text>
                   {showCode && (
                     <Email_check_box>
-                      <Email_check_input
-                        placeholder="발송된 인증 코드를 입력해주세요"
-                        value={authcode}
-                        onChange={(e) => setAuthcode(e.target.value)}
-                      ></Email_check_input>
-                      <Check_timer>{formatTime(timeleft)}</Check_timer>
+                      <Email_check_con>
+                        <Email_check_input
+                          placeholder="발송된 인증 코드를 입력해주세요"
+                          value={authcode}
+                          onChange={(e) => setAuthcode(e.target.value)}
+                        ></Email_check_input>
+                        <Check_timer>{formatTime(timeleft)}</Check_timer>
+                      </Email_check_con>
+                      <Email_check_btn2 onClick={check_Code}>
+                        확인
+                      </Email_check_btn2>
                     </Email_check_box>
                   )}
+                  <Email_check_text>{checkMessage}</Email_check_text>
                 </Email_box>
                 <Password_box>
                   <Password_title>비밀번호</Password_title>
@@ -123,6 +175,7 @@ const SignUp = () => {
                       alt="비밀번호 표시"
                     />
                   </Password_input>
+                  <Email_check_text>{pwMessage}</Email_check_text>
                 </Password_box>
                 <Password_box>
                   <Password_title>비밀번호 확인</Password_title>
@@ -141,6 +194,7 @@ const SignUp = () => {
                       alt="비밀번호 표시"
                     />
                   </Password_input>
+                  <Email_check_text>{checkPwMessage}</Email_check_text>
                 </Password_box>
 
                 <Btn_box>
@@ -233,11 +287,11 @@ const Login_title = styled.p`
 const Email_box = styled.div`
   width: 392px;
   height: auto;
-  gap: 16px;
   display: flex;
   flex-direction: column;
   align-items: baseline;
   margin-bottom: 16px;
+  position: relative;
 `;
 const Email_title = styled.p`
   color: #575141;
@@ -268,6 +322,12 @@ const Email_text = styled.input`
     outline: none;
   }
 `;
+const Email_check_text = styled.p`
+  font-size: 12px;
+  font-weight: 500;
+  padding: 2px 4px;
+  color: #e21414;
+`;
 const Email_check_btn = styled.button`
   width: 100px;
   height: 40px;
@@ -285,8 +345,13 @@ const Email_check_btn = styled.button`
   }
 `;
 const Email_check_box = styled.div`
-  width: 392px;
+  width: 100%;
   height: 39px;
+  display: flex;
+  gap: 12px;
+`;
+const Email_check_con = styled.div`
+  width: 316px;
   border: 1px solid #bdbdbd;
   padding: 0 16px;
   font-size: 16px;
@@ -294,6 +359,7 @@ const Email_check_box = styled.div`
   border-radius: 12px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   overflow: hidden;
   box-sizing: border-box;
   ::placeholder {
@@ -304,7 +370,7 @@ const Email_check_box = styled.div`
   }
 `;
 const Email_check_input = styled.input`
-  width: 360px;
+  width: 316px;
   height: 100%;
   border: none;
   font-size: 16px;
@@ -325,6 +391,22 @@ const Check_timer = styled.p`
   height: 42px;
   margin-bottom: 2px;
   color: #daa005;
+`;
+const Email_check_btn2 = styled.button`
+  width: 64px;
+  height: 40px;
+  border: none;
+  border-radius: 12px;
+  color: #575141;
+  background-color: #fcd671;
+  font-size: 16px;
+  font-weight: 600;
+  transition: 0.1s ease-in;
+  :hover {
+    color: white;
+    background-color: #daa005;
+    cursor: pointer;
+  }
 `;
 const Password_box = styled.div`
   width: 392px;
