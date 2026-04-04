0679d83df147658158d9f857679643b80adec960
강세아 <gangsea817@gmail.com>
2026-04-05 02:51:54 +0900

    feat: 일기 목록 조회 기능 추가

--- src/apis/diaries.js
+++ src/apis/diaries.js
@@ -22,6 +22,17 @@ export const getDiaries = async (id) => {
   }
 };
 
+// 일기 목록 조회
+export const getDiariesList = async (params) => {
+  try {
+    const res = await instance.get(`/api/v1/diaries`, { params });
+    return res.data;
+  } catch (error) {
+    console.error("일기 목록 조회 실패:", error);
+    throw error;
+  }
+};
+
 // 일기 내용 수정
 export const updateDiaries = async (id, updateData) => {
   try {
